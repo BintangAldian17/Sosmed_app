b2d969dd34e1c05298213f34d57e11738cc8659f
BintangAldian17 <bintangaldian17@gmail.com>
2023-05-22 20:16:00 +0700

    setting env

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv"
 
 dotenv.config({ path: "./.env" })
 
-const db = new Sequelize("posts_db", "root", process.env.PASSWORD_DB, {
-    host: "localhost",
-    dialect: "mysql"
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.PASSWORD_DB, {
+    host: process.env.HOST,
+    dialect: "mysql",
+    port: process.env.PORT
 })
 
 export default db
