044c45a579c5108d75517fbc7008129af4d7bf5a
BintangAldian17 <bintangaldian17@gmail.com>
2023-05-24 20:52:02 +0700

    upload file

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static(path.join(__dirname + './uploads')));
+app.use('/uploads', express.static('uploads'));
 app.use(router)
 
 const server = app.listen(port, () => {
