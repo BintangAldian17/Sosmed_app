0817b9bcd155ac80af70ff67627dd0ceb145fd99
BintangAldian17 <bintangaldian17@gmail.com>
2023-05-24 20:47:38 +0700

    upload file

--- backend/controller/PostsController/Posts.js
+++ backend/controller/PostsController/Posts.js
@@ -86,7 +86,7 @@ export const deletePost = async (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "../../uploads")
+        cb(null, "/uploads")
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}${file.originalname}`)
